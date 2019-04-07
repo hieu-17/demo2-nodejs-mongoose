23d04c790bfb2501f1c17f929b0f6d6636666585
hieu <hieumaster17@gmail.com>
2019-04-08 01:29:22 +0700

    delete template

--- template/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/auth.controller')
-
-
-router.get('/login',controller.login)
-router.post('/login',controller.postLogin)
-
-module.exports = router;
